6f2cf6b6e77891eac6733969ce6f067601285b21
reina258 <hanrui_wei@163.com>
2024-09-25 18:08:13 +0800

    Add files via upload

---
+++ tips.py
@@ -0,0 +1,6 @@
+import os
+print(os.getcwd())
+# I've already modified the csv path on my laptop
+# But it still throw an error when using someone else's computer
+# If it does, could you please cd into the correct path? 
+# I really appreciate!
